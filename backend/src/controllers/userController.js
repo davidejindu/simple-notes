fd201f5ddca1992d43633c5b16515abc05d7cf2c
David Ejindu <ejindudavid540@gmail.com>
2025-06-08 23:41:51 -0400

    Fix: enable secure cross-origin cookie auth

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -49,7 +49,7 @@ export const loginUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "None",
       maxAge: 365 * 24 * 60 * 60 * 1000, 
     });
 
