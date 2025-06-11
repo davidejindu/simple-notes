74b13a16eac1c7188d4aafb6aa5e448b5bb05547
David Ejindu <ejindudavid540@gmail.com>
2025-06-10 22:26:03 -0400

    change secure to true

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -48,7 +48,7 @@ export const loginUser = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
       sameSite: "None",
       maxAge: 365 * 24 * 60 * 60 * 1000, 
     });
