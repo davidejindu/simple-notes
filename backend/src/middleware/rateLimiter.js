2ee409814fff09bb1b00a239bb3e73f4a0fbb364
David Ejindu <ejindudavid540@gmail.com>
2025-06-10 22:01:41 -0400

    Add rate limiter logging

--- backend/src/middleware/rateLimiter.js
+++ backend/src/middleware/rateLimiter.js
@@ -1,23 +1,21 @@
 
 import ratelimit from "../config/upstash.js";
 
-const rateLimiter = async (req,res,next) => {
-
+const rateLimiter = async (req, res, next) => {
     try {
-        const {success} = await ratelimit.limit("my-rate-limit");
-
-        if(!success) {
-            return res.status(429).json({message:"To many requests, please try again later"});
-        }
-
-        next();
-
+      const { success } = await ratelimit.limit("my-rate-limit");
+  
+      if (!success) {
+        console.log("🚫 Rate limit triggered for request:", req.originalUrl);
+        return res.status(429).json({ message: "Too many requests" });
+      }
+  
+      next();
     } catch (error) {
-        console.log("Rate limit error", error);
-        next(error);
-
+      console.log("Rate limit error", error);
+      next(error);
     }
-
-};
+  };
+  
 
 export default rateLimiter;
