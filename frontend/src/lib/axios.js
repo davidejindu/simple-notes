3736d9598e3decb5c414d0c078cf1684a1b999cf
David Ejindu <ejindudavid540@gmail.com>
2025-06-10 23:00:36 -0400

    replace api helper function

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,22 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api`,
-  withCredentials: true,
+  withCredentials: true, // This sends cookies
 });
+
+// Request interceptor to add Authorization header
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+      console.log('Adding token to request:', token.substring(0, 20) + '...');
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
